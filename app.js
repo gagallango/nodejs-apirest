e952f0200a07f5478dc5dc25a6451e65df86d480
Gabriela Gallango <ga.gallango@gmail.com>
2020-06-09 11:49:33 +0200

    add alltuits and tuitsdetails

--- app.js
+++ app.js
@@ -20,5 +20,6 @@ require('./configs/locals.config')(app)
 // Base URLS
 app.use('/', require('./routes/index.routes'))
 app.use('/', require('./routes/auth.routes'))
+app.use('/tuits', require('./routes/tuits.routes'))
 
 module.exports = app
--- routes/tuits.routes.js
+++ routes/tuits.routes.js
@@ -3,8 +3,20 @@ const router = express.Router()
 const User = require('./../models/user.model')
 const Tuit = require('./../models/tuit.model')
 
+
 //GET ALL TUITS
-router.get('/tuits', (req, res, next) => {
+router.get('/', (req, res, next) => {
+    Tuit.find()
+        .then(allTuits => res.render('tuits/tuits', { tuits: allTuits }))
+        .catch(err => console.log('Error', err))
+})
+
+//COASTERS DETS
+router.get('/:id', (req, res, next) => {
+    Tuit.findById(req.params.id)
+        .populate('user')
+        .then(tuitDetails => res.render('tuits/tuit-det', tuitDetails))
+        .catch(err => console.log('Error', err))
 
 })
 
